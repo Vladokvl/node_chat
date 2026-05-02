10878244075799e59a09c317b90c66fb4e1b6cfa
Vlad Zhovtani <zovtanyy@gmail.com>
2026-05-02 16:35:47 +0300

    done

---
+++ src/models/Message.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Message = sequelize.define('Message', {
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+
+module.exports = Message;
